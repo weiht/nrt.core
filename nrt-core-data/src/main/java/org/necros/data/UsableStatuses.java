2d2f81e5d4ba34c4a7b485771ea981e7916b97a9
weiht <weihthchk@gmail.com>
2013-10-19 20:50:56 +0800

    General data usable status strings added.

---
+++ nrt-core-data/src/main/java/org/necros/data/UsableStatuses.java
@@ -0,0 +1,9 @@
+package org.necros.data;
+
+public class UsableStatuses {
+	private UsableStatuses() {}
+	
+	public static final String USABLE = "usable";
+	public static final String DELETED = "deleted";
+	public static final String DISABLED = "disabled";
+}
