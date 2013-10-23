99ffcf4b17b591e557a70b76ff448be67e299523
weiht <weihthchk@hotmail.com>
2013-10-23 13:56:49 +0800

    Meta classes defined.

--- nrt-core-data/src/main/java/org/necros/data/MetaClass.java
+++ nrt-core-data/src/main/java/org/necros/data/MetaClass.java
@@ -1,7 +1,79 @@
 package org.necros.data;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class MetaClass implements Serializable {
+	private String id;
+	private String metaPackage;
+	private String name;
+	private String displayName;
+	private String tableName;
+	private String revision;
+	private String remarks;
+	private List<MetaProperty> properties;
 	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getMetaPackage() {
+		return metaPackage;
+	}
+
+	public void setMetaPackage(String metaPackage) {
+		this.metaPackage = metaPackage;
+	}
+	
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getTableName() {
+		return tableName;
+	}
+
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+	
+	public String getRevision() {
+		return revision;
+	}
+
+	public void setRevision(String revision) {
+		this.revision = revision;
+	}
+	
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+		
+	public List<MetaProperty> getProperties() {
+		return properties;
+	}
+
+	public void setProperties(List<MetaProperty> properties) {
+		this.properties = properties;
+	}	
 }
--- nrt-core-data/src/main/java/org/necros/data/MetaProperty.java
+++ nrt-core-data/src/main/java/org/necros/data/MetaProperty.java
@@ -3,5 +3,93 @@ package org.necros.data;
 import java.io.Serializable;
 
 public class MetaProperty implements Serializable {
+	private String id;
+	private MetaClass metaClass;
+	private String name;
+	private String displayName;
+	private String columnName;
+	private String inputHint;
+	private String displayType;
+	private String dataType;
+	private String remarks;
+	private String usableStatus;
 	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public MetaClass getMetaClass() {
+		return metaClass;
+	}
+
+	public void setMetaClass(MetaClass metaClass) {
+		this.metaClass = metaClass;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDisplayName() {
+		return displayName;
+	}
+	
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public String getColumnName() {
+		return columnName;
+	}
+
+	public void setColumnName(String columnName) {
+		this.columnName = columnName;
+	}
+	
+	public String getInputHint() {
+		return inputHint;
+	}
+
+	public void setInputHint(String inputHint) {
+		this.inputHint = inputHint;
+	}
+	
+	public String getDisplayType() {
+		return displayType;
+	}
+
+	public void setDisplayType(String displayType) {
+		this.displayType = displayType;
+	}
+	
+	public String getDataType() {
+		return dataType;
+	}
+
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+	}
+
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+	
+	public String getUsableStatus() {
+		return usableStatus;
+	}
+	public void setUsableStatus(String usableStatus) {
+		this.usableStatus = usableStatus;
+	}
 }
