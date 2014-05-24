584499a2d2428c0bfd80d127225c062d56197dae
weiht <weihthchk@gmail.com>
2014-05-24 10:06:35 +0800

    Abstract ScriptRunner added.

--- nrt-core-scripting/src/main/java/org/necros/scripting/AbstractScriptRunner.java
+++ nrt-core-scripting/src/main/java/org/necros/scripting/AbstractScriptRunner.java
@@ -1,11 +1,14 @@
 package org.necros.scripting;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 import javax.script.ScriptException;
 
 import org.necros.res.ResourceProvider;
+import org.necros.util.ResourceUtils;
+import org.necros.util.StringUtils;
 
 public abstract class AbstractScriptRunner
 implements ScriptRunner {
@@ -15,9 +18,32 @@ implements ScriptRunner {
 	public Map<String, Object> runScript(String script,
 			Map<String, Object> extraContext) throws ScriptException,
 			IOException {
-		// TODO Auto-generated method stub
-		return null;
+		if (StringUtils.isEmpty(script)) {
+			throw new IOException("No script specified.");
+		}
+		if (resourceProvider == null) return null;
+		String name = scriptName(script);
+		InputStream ins = resourceProvider.read(name);
+		try {
+			return doRunScript(ins, extraContext);
+		} finally {
+			ResourceUtils.closeStream(ins);
+		}
 	}
+
+	private String scriptName(String script) {
+		String ext = getScriptExtensionName();
+		if (StringUtils.isEmpty(ext)) {
+			return script;
+		}
+		return StringUtils.changeFileExtension(script, ext);
+	}
+
+	protected abstract Map<String, Object> doRunScript(InputStream ins,
+			Map<String, Object> extraContext)
+					throws ScriptException, IOException;
+	
+	public abstract String getScriptExtensionName();
 
 	public void setResourceProvider(ResourceProvider resourceProvider) {
 		this.resourceProvider = resourceProvider;
