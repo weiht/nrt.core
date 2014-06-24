d49da17e279321d85aa19407802957477e0aea6b
weiht <weihthchk@gmail.com>
2014-06-24 23:18:06 +0800

    ApplicationContext injected to ScriptRunner implementations.

--- nrt-core-scripting/src/main/java/org/necros/scripting/AbstractScriptRunner.java
+++ nrt-core-scripting/src/main/java/org/necros/scripting/AbstractScriptRunner.java
@@ -2,6 +2,7 @@ package org.necros.scripting;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.script.ScriptException;
@@ -9,10 +10,17 @@ import javax.script.ScriptException;
 import org.necros.res.ResourceProvider;
 import org.necros.util.ResourceUtils;
 import org.necros.util.StringUtils;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 public abstract class AbstractScriptRunner
-implements ScriptRunner {
+implements ScriptRunner, ApplicationContextAware {
+	public static final String KEY_APPLICATION_CONTEXT = "applicationContext";
+	
 	protected ResourceProvider resourceProvider;
+	protected ApplicationContext applicationContext;
+	private Map<String, Object> rootBindings;
 
 	@Override
 	public Map<String, Object> runScript(String script,
@@ -25,11 +33,25 @@ implements ScriptRunner {
 		String name = scriptName(script);
 		InputStream ins = resourceProvider.read(name);
 		if (ins == null) return extraContext;
+		if (extraContext == null) {
+			extraContext = new HashMap<String, Object>(ensureRootBindings());
+		} else {
+			extraContext.putAll(ensureRootBindings());
+		}
 		try {
 			return doRunScript(ins, extraContext);
 		} finally {
 			ResourceUtils.closeStream(ins);
 		}
+	}
+
+	private Map<String, Object> ensureRootBindings() {
+		if (rootBindings == null) {
+			rootBindings = new HashMap<String, Object>();
+			rootBindings.put(KEY_APPLICATION_CONTEXT, applicationContext);
+			//TODO Do some extra initialization
+		}
+		return rootBindings;
 	}
 
 	private String scriptName(String script) {
@@ -48,5 +70,11 @@ implements ScriptRunner {
 
 	public void setResourceProvider(ResourceProvider resourceProvider) {
 		this.resourceProvider = resourceProvider;
+	}
+	
+	@Override
+	public void setApplicationContext(ApplicationContext ctx)
+			throws BeansException {
+		this.applicationContext = ctx;
 	}
 }
