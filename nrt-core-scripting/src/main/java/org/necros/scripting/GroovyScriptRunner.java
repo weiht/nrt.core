1313f5c99556cc4cbf4ae651366cd74ea2b77244
weiht <weihthchk@gmail.com>
2014-06-24 23:18:30 +0800

    Groovy script runner use repository paths as classpaths.

--- nrt-core-scripting/src/main/java/org/necros/scripting/GroovyScriptRunner.java
+++ nrt-core-scripting/src/main/java/org/necros/scripting/GroovyScriptRunner.java
@@ -3,17 +3,29 @@ package org.necros.scripting;
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.script.ScriptException;
 
+import org.codehaus.groovy.control.CompilerConfiguration;
+import org.necros.res.repo.RepositoryLocator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class GroovyScriptRunner
 extends AbstractScriptRunner {
+	private static final Logger logger = LoggerFactory.getLogger(GroovyScriptRunner.class);
 
 	private static final String GROOVY_FILE_EXTENSION = ".groovy";
+	
+	private RepositoryLocator repositoryLocator;
+	private List<String> classpaths;
 
 	@Override
 	protected Map<String, Object> doRunScript(InputStream ins,
@@ -31,14 +43,31 @@ extends AbstractScriptRunner {
 
 	private GroovyShell getShell(Map<String, Object> extraContext) {
 		Binding bindings = new Binding(extraContext);
-		GroovyShell shell = new GroovyShell(bindings);
+		CompilerConfiguration cconf = new CompilerConfiguration();
+		cconf.setClasspathList(ensureClasspaths());
+		GroovyShell shell = new GroovyShell(bindings, cconf);
 		bindings.setVariable("shell", shell);
 		bindings.setVariable("resourceProvider", resourceProvider);
 		return shell;
 	}
 
+	private List<String> ensureClasspaths() {
+		if (classpaths == null) {
+			classpaths = new ArrayList<String>();
+			for (File f: repositoryLocator.getRepositories()) {
+				classpaths.add(f.getAbsolutePath());
+			}
+			logger.trace("Groovy script classpaths: {}", classpaths);
+		}
+		return classpaths;
+	}
+
 	@Override
 	public String getScriptExtensionName() {
 		return GROOVY_FILE_EXTENSION;
+	}
+
+	public void setRepositoryLocator(RepositoryLocator repositoryLocator) {
+		this.repositoryLocator = repositoryLocator;
 	}
 }
